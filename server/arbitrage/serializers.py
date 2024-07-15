80ba02946dfc3c9f4f9a05eaa7610cdb167d4ae4
unknown <hazardovic1@gmail.com>
2024-07-15 17:33:48 +0300

    removed frontend

--- server/arbitrage/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ArbitrageOpportunity
-
-class ArbitrageOpportunitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ArbitrageOpportunity
-        fields = '__all__'
--- server/arbitrage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("all/", views.get_all_arbitrages, name='list_all_arbitrages'),
-    path("active/", views.get_active_arbitrages, name='list_active_arbitrages'),
-    path("inactive/", views.get_inactive_arbitrages, name='list_historical_arbitrages'),
-]
